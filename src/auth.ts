604bafd7f5d0c8d940af53f736a3a9cf48dc5a6c
Arsen Gabrielyan <105947988+ArsenGabrielyan@users.noreply.github.com>
2025-04-30 18:39:02 +0400

    Fixed another bug after linking account

--- src/auth.ts
+++ src/auth.ts
@@ -21,7 +21,13 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       await db.user.update({
         where: {id: user.id},
         data: {
-          emailVerified: new Date()
+          emailVerified: new Date(),
+          cvPageSettings: {
+            showEmail: true,
+            showAddress: true,
+            showLinks: true,
+            showPhone: true
+          }
         }
       })
       await logAction({
